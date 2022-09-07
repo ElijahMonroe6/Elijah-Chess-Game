bde6cb6f412341d3165cb7c00f82a1df9762d8dc
Elijah Monroe <emonroe6@gmu.edu>
2022-09-06 23:31:57 -0400

    Added comments to Piece header file and made it so isValidMove checks for itself looking through the board

--- Chess/Piece.cpp
+++ Chess/Piece.cpp
@@ -128,20 +128,20 @@ Piece* Piece::createPiece(PieceType pieceType, int color, pair<int, int> positio
 
 };
 
-King::King() : Piece(), hasMoved(false) {};
-King::King(int color, int x, int y) : Piece(color, x, y), hasMoved(false) {};
-King::King(int color, std::pair<int, int> position) : Piece(color, position), hasMoved(false) {};
+King::King() : Piece(), hasMovedPosition(false) {};
+King::King(int color, int x, int y) : Piece(color, x, y), hasMovedPosition(false) {};
+King::King(int color, std::pair<int, int> position) : Piece(color, position), hasMovedPosition(false) {};
 
 bool King::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 	pair<int, int> curPosition = this->getPosition();
-	pair<int, int> deltaEnd = HelperFunctions::delta(curPosition, endPosition);
+	pair<int, int> deltaToEnd = HelperFunctions::delta(curPosition, endPosition);
 
-	if (abs(deltaEnd.first) > 1 || abs(deltaEnd.second) > 1) return false;
+	if (abs(deltaToEnd.first) > 1 || abs(deltaToEnd.second) > 1) return false;
 
 	for (Piece* piece : board) {
 
-		if (endPosition == piece->getPosition() && this->getColor() == piece->getColor()) return false;
+		if (endPosition == piece->getPosition() && this->getColor() == piece->getColor() && this != piece) return false;
 
 	}
 
@@ -156,18 +156,22 @@ Queen::Queen(int color, std::pair<int, int> position) : Piece(color, position) {
 bool Queen::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 	pair<int, int> curPosition = this->getPosition();
-	pair<int, int> deltaEnd = HelperFunctions::delta(curPosition, endPosition);
+	pair<int, int> deltaToEnd = HelperFunctions::delta(curPosition, endPosition);
 
-	if (abs(deltaEnd.first) != abs(deltaEnd.second) && deltaEnd.first && deltaEnd.second) return false;
+	if (abs(deltaToEnd.first) != abs(deltaToEnd.second) && deltaToEnd.first && deltaToEnd.second) return false;
 
 	for (Piece* piece : board) {
 
-		pair<int, int> piecePosition = piece->getPosition();
-		pair<int, int> deltaPiece = HelperFunctions::delta(curPosition, piecePosition);
-		bool isParallel = acos(HelperFunctions::dot(deltaEnd, deltaPiece) / (HelperFunctions::magnitude(deltaEnd) * HelperFunctions::magnitude(deltaPiece))) == 0;//This doesn't really check if it is parallel, it actually checks if the angle between the vectors is zero which is a subset of parallel
+		if (this != piece) {
 
-		if (endPosition != piecePosition && isParallel) return false;
-		if (this->getColor() == piece->getColor() && isParallel) return false;
+			pair<int, int> piecePosition = piece->getPosition();
+			pair<int, int> deltaToPiece = HelperFunctions::delta(curPosition, piecePosition);
+			bool isParallel = acos(HelperFunctions::dot(deltaToEnd, deltaToPiece) / (HelperFunctions::magnitude(deltaToEnd) * HelperFunctions::magnitude(deltaToPiece))) == 0;//This doesn't really check if it is parallel, it actually checks if the angle between the vectors is zero which is a subset of parallel
+
+			if (endPosition != piecePosition && isParallel) return false;
+			if (this->getColor() == piece->getColor() && isParallel) return false;
+
+		}
 
 	}
 
@@ -182,18 +186,22 @@ Bishop::Bishop(int color, std::pair<int, int> position) : Piece(color, position)
 bool Bishop::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 	pair<int, int> curPosition = this->getPosition();
-	pair<int, int> deltaEnd = HelperFunctions::delta(curPosition, endPosition);
+	pair<int, int> deltaToEnd = HelperFunctions::delta(curPosition, endPosition);
 
-	if (abs(deltaEnd.first) != abs(deltaEnd.second)) return false;
+	if (abs(deltaToEnd.first) != abs(deltaToEnd.second)) return false;
 
 	for (Piece* piece : board) {
 
-		pair<int, int> piecePosition = piece->getPosition();
-		pair<int, int> deltaPiece = HelperFunctions::delta(curPosition, piecePosition);
-		bool isParallel = acos(HelperFunctions::dot(deltaEnd, deltaPiece) / (HelperFunctions::magnitude(deltaEnd) * HelperFunctions::magnitude(deltaPiece))) == 0;//This doesn't really check if it is parallel, it actually checks if the angle between the vectors is zero which is a subset of parallel
+		if (this != piece) {
 
-		if (endPosition != piecePosition && isParallel) return false;
-		if (this->getColor() == piece->getColor() && isParallel) return false;
+			pair<int, int> piecePosition = piece->getPosition();
+			pair<int, int> deltaToPiece = HelperFunctions::delta(curPosition, piecePosition);
+			bool isParallel = acos(HelperFunctions::dot(deltaToEnd, deltaToPiece) / (HelperFunctions::magnitude(deltaToEnd) * HelperFunctions::magnitude(deltaToPiece))) == 0;//This doesn't really check if it is parallel, it actually checks if the angle between the vectors is zero which is a subset of parallel
+
+			if (endPosition != piecePosition && isParallel) return false;
+			if (this->getColor() == piece->getColor() && isParallel) return false;
+
+		}
 
 	}
 
@@ -214,9 +222,13 @@ bool Knight::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 	for (Piece* piece : board) {
 
-		pair<int, int> piecePosition = piece->getPosition();
+		if (this != piece) {
 
-		if (endPosition == piecePosition && this->getColor() == piece->getColor()) return false;
+			pair<int, int> piecePosition = piece->getPosition();
+
+			if (endPosition == piecePosition && this->getColor() == piece->getColor()) return false;
+
+		}
 
 	}
 
@@ -224,25 +236,29 @@ bool Knight::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 };
 
-Rook::Rook() : Piece(), hasMoved(false) {};
-Rook::Rook(int color, int x, int y) : Piece(color, x, y), hasMoved(false) {};
-Rook::Rook(int color, std::pair<int, int> position) : Piece(color, position), hasMoved(false) {};
+Rook::Rook() : Piece(), hasMovedPosition(false) {};
+Rook::Rook(int color, int x, int y) : Piece(color, x, y), hasMovedPosition(false) {};
+Rook::Rook(int color, std::pair<int, int> position) : Piece(color, position), hasMovedPosition(false) {};
 
 bool Rook::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 	pair<int, int> curPosition = this->getPosition();
-	pair<int, int> deltaEnd = HelperFunctions::delta(curPosition, endPosition);
+	pair<int, int> deltaToEnd = HelperFunctions::delta(curPosition, endPosition);
 
-	if (deltaEnd.first != 0 && deltaEnd.second != 0) return false;
+	if (deltaToEnd.first != 0 && deltaToEnd.second != 0) return false;
 
 	for (Piece* piece : board) {
 
-		pair<int, int> piecePosition = piece->getPosition();
-		pair<int, int> deltaPiece = HelperFunctions::delta(curPosition, piecePosition);
-		bool isParallel = acos(HelperFunctions::dot(deltaEnd, deltaPiece) / (HelperFunctions::magnitude(deltaEnd) * HelperFunctions::magnitude(deltaPiece))) == 0;//This doesn't really check if it is parallel, it actually checks if the angle between the vectors is zero which is a subset of parallel
+		if (this != piece) {
 
-		if (endPosition != piecePosition && isParallel) return false;
-		if (this->getColor() == piece->getColor() && isParallel) return false;
+			pair<int, int> piecePosition = piece->getPosition();
+			pair<int, int> deltaToPiece = HelperFunctions::delta(curPosition, piecePosition);
+			bool isParallel = acos(HelperFunctions::dot(deltaToEnd, deltaToPiece) / (HelperFunctions::magnitude(deltaToEnd) * HelperFunctions::magnitude(deltaToPiece))) == 0;//This doesn't really check if it is parallel, it actually checks if the angle between the vectors is zero which is a subset of parallel
+
+			if (endPosition != piecePosition && isParallel) return false;
+			if (this->getColor() == piece->getColor() && isParallel) return false;
+
+		}
 
 	}
 
@@ -250,22 +266,26 @@ bool Rook::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 };
 
-Pawn::Pawn() : Piece(), hasMoved(false) {};
-Pawn::Pawn(int color, int x, int y) : Piece(color, x, y), hasMoved(false) {};
-Pawn::Pawn(int color, std::pair<int, int> position) : Piece(color, position), hasMoved(false) {};
+Pawn::Pawn() : Piece(), hasMovedPosition(false) {};
+Pawn::Pawn(int color, int x, int y) : Piece(color, x, y), hasMovedPosition(false) {};
+Pawn::Pawn(int color, std::pair<int, int> position) : Piece(color, position), hasMovedPosition(false) {};
 
 bool Pawn::isValidMove(list<Piece*> board, pair<int, int> endPosition) {
 
 	pair<int, int> curPosition = this->getPosition();
-	pair<int, int> deltaEnd = HelperFunctions::delta(curPosition, endPosition);
+	pair<int, int> deltaToEnd = HelperFunctions::delta(curPosition, endPosition);
 
-	if (deltaEnd.first != 0 || deltaEnd.second != this->getColor()) return false;
+	if (deltaToEnd.first != 0 || deltaToEnd.second != this->getColor()) return false;
 
 	for (Piece* piece : board) {
 
-		pair<int, int> piecePosition = piece->getPosition();
+		if (this != piece) {
 
-		if (endPosition == piecePosition) return false;
+			pair<int, int> piecePosition = piece->getPosition();
+
+			if (endPosition == piecePosition) return false;
+
+		}
 
 	}
 
--- Chess/Piece.h
+++ Chess/Piece.h
@@ -5,97 +5,127 @@ enum class PieceType {KING,QUEEN,BISHOP,KNIGHT,ROOK,PAWN};
 
 class Piece {
 
-	int color;//should be 1 for white, -1 for black; also used for direction of movement
+	//Piece Private Members
+	int color;//should be 1 for white, -1 for black; also used for direction of movement of pawns
 	std::pair<int, int> position;
 
 public:
+	//Piece Getters and Setters
 	int getColor() { return this->color; };
 	void setColor(int color) { this->color = color; };
 	std::pair<int, int> getPosition() { return this->position; };
 	void setPosition() { this->position = position; };
 
+	//Piece Constructors
 	Piece();
 	Piece(int isWhite, int x, int y);
 	Piece(int isWhite, std::pair<int, int> position);
-	virtual bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) = 0;
+
+	//Piece Public Methods
+	virtual bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) = 0;//checks if the endPosition would be a valid move given the board; Assumes that endPosition is within the board and isn't the same as the current position
 	
-	static Piece* createPiece(PieceType pieceType);
-	static Piece* createPiece(PieceType pieceType, int isWhite, int x, int y);
-	static Piece* createPiece(PieceType pieceType, int isWhite, std::pair<int, int> position);
+	//Piece Public Static Class Methods
+	static Piece* createPiece(PieceType pieceType);//Use this to construct pieces so that there is no conversion issues
+	static Piece* createPiece(PieceType pieceType, int color, int x, int y);//Use this to construct pieces so that there is no conversion issues; allows you to set color of piece and position
+	static Piece* createPiece(PieceType pieceType, int color, std::pair<int, int> position);//Use this to construct pieces so that there is no conversion issues; allows you to set color of piece and position
 
 };
 
 class King : public Piece {
 
-	bool hasMovedPosition;
+	//King Private Members
+	bool hasMovedPosition;//is false if the king hasn't moved yet, true otherwise; really only useful for castles
 
 public:
-	bool hasMoved() { return this->hasMovedPosition; };
-	void itMoved() { this->hasMovedPosition = true; }
+	//King Getters and Setters
+	bool hasMoved() { return this->hasMovedPosition; };//returns whether the king has moved yet; really only useful for castles
+	void itMoved() { this->hasMovedPosition = true; }//sets hasMovedPosition to true; really only useful for castles
 
+	//King Constructors
 	King();
 	King(int color, int x, int y);
 	King(int color, std::pair<int, int> position);
-	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;
+
+	//King Public Methods
+	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;//Kings can move one square away from itself, and castles; read comment for Piece for more info on method
 
 };
 
 class Queen : public Piece {
 
 public:
+	//Queen Constructors
 	Queen();
 	Queen(int color, int x, int y);
 	Queen(int color, std::pair<int, int> position);
-	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;
+
+	//Queen Public Methods
+	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;//Queens can move diagonally, vertically, and horizontally; read comment for Piece for more info on method
 
 };
 
 class Bishop : public Piece {
 
 public:
+	//Bishop Constructors
 	Bishop();
 	Bishop(int color, int x, int y);
 	Bishop(int color, std::pair<int, int> position);
-	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;
+
+	//Bishop Public Methods
+	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;//Bishops can move diagonally; read comment for Piece for more info on method
 
 };
 
 class Knight : public Piece {
 
 public:
+	//Knight Constructors
 	Knight();
 	Knight(int color, int x, int y);
 	Knight(int color, std::pair<int, int> position);
-	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;
+
+	//Knight Public Methods
+	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;//Knights can move in L shape through pieces; read comment for Piece for more info on method
 
 };
 
 class Rook : public Piece {
 
-	bool hasMovedPosition;
+	//Rook Private Members
+	bool hasMovedPosition;//is false if the rook hasn't moved yet, true otherwise; really only useful for castles
 
 public:
-	bool hasMoved() { return this->hasMovedPosition; };
-	void itMoved() { this->hasMovedPosition = true; }
+	//Rook Getters and Setters
+	bool hasMoved() { return this->hasMovedPosition; };//returns whether the rook has moved yet; really only useful for castles
+	void itMoved() { this->hasMovedPosition = true; }//sets hasMovedPosition to true; really only useful for castles
 
+	//Rook Constructors
 	Rook();
 	Rook(int color, int x, int y);
 	Rook(int color, std::pair<int, int> position);
-	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;
+
+	//Rook Public Methods
+	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;//Rooks can move vertically, horizontally, and castles; read comment for Piece for more info on method
 
 };
 
 class Pawn : public Piece {
 
+	//Pawn Private Members
 	bool hasMovedPosition;
 
 public:
+	//Pawn Getters and Setters
 	bool hasMoved() { return this->hasMovedPosition; };
 	void itMoved() { this->hasMovedPosition = true; }
 
+	//Pawn Constructors
 	Pawn();
 	Pawn(int color, int x, int y);
 	Pawn(int color, std::pair<int, int> position);
-	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;
+
+	//Pawn Public Methods
+	bool isValidMove(std::list<Piece*> board, std::pair<int, int> endPosition) override;//Pawns can move forward one space, en passant, and two spaces on first move; read comment for Piece for more info on method
 
 };
